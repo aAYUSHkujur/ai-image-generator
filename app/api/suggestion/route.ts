e98a92783668b58e18d7f5ba5400bda6d4edcad9
aayushk720@gmail.com <aayushk720@gmail.com>
2023-04-27 13:45:40 +0530

    added azure

--- app/api/suggestion/route.ts
+++ app/api/suggestion/route.ts
@@ -1,6 +1,6 @@
 export async function GET(request: Request) {
     // Connect to Microsoft Azure Function endpoint
-    const response = await fetch('...', {
+    const response = await fetch("http://localhost:7071/api/getChatGPTSuggestion", {
         cache: "no-store"
     });
 
